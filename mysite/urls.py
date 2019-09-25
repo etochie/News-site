00da1c98a20879286154f25430bade3d1856831e
etochie <etochie@gmail.com>
2019-09-26 00:53:17 +0700

    added images for articles(media)

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 from mysite import views
 
 urlpatterns = [
@@ -23,3 +24,6 @@ urlpatterns = [
     path('news/', include('news.urls'), name='news_page'),
     path('admin/', admin.site.urls, name='admin_page'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- news/models.py
+++ news/models.py
@@ -11,15 +11,20 @@ def gen_slug(s):
     time_slug = str(int(time()))
     return new_slug + '-' + time_slug
 
+
+# def gen_img_name(s):
+#     img_name = slugify(s)
+#     return img_name + '.jpg'
+
+
 class Article(models.Model):
     title = models.CharField(max_length=200)
     title2 = models.CharField(max_length=300, blank=True)
-    img = models.ImageField(upload_to='images/', blank=True)
+    img = models.ImageField(blank=True)
     slug = models.SlugField(max_length=100, blank=True, unique=True)
     body = models.TextField()
     tag = models.ManyToManyField('Tag', blank=False, related_name='posts')
     pub_date = models.DateTimeField(default=timezone.now, blank=False)
-
 
     class Meta:
         ordering = ['-pub_date']
@@ -30,6 +35,9 @@ class Article(models.Model):
     def save(self, *args, **kwargs):
         if not self.id:
             self.slug = gen_slug(self.title[:20])
+        # if self.img:
+        #     name = gen_img_name(self.title[:15])
+        #     self.img.name = name
         return super().save(*args, **kwargs)
 
     def get_detail_url(self):
